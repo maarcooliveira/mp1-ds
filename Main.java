9b32860b227c37bebc3abbfdb67cd2da1daab408
Cassio Sousa <cassiosantossousa@gmail.com>
2015-03-14 20:37:29 -0500

    Refactored Main

--- Main.java
+++ Main.java
@@ -1,13 +1,30 @@
 import java.io.IOException;
 
 /**
- * Created by Marco Andre De Oliveira <mdeoliv2@illinois.edu>
- * Date: 3/14/15
+ * Creates a new server that listens to a given port of a given address. Initial delay is also set.
+ *
+ * @author Marco Andre De Oliveira <mdeoliv2@illinois.edu>
+ * @version 1.0
+ * @since 3/14/15
  */
 public class Main {
 
-    // java Main name address port delay
+    /**
+     * Creates a new server. The delay unit is seconds.
+     *
+     * Usage (in the terminal): java Main [serverName] [IpAddress] [port] [delay]
+     * Example: java Main A localhost 8080 2
+     *
+     * @param args all arguments required to create a server (4 in total).
+     * @throws IOException if tried to listen to a port or address that does not exist.
+     */
     public static void main(String[] args) throws IOException {
+
+        if (args.length != 4) {
+            System.out.println("Usage: java Main [serverName] [IpAddress] [port] [delay]");
+            return;
+        }
+
         String name = args[0];
         String address = args[1];
         int port = Integer.valueOf(args[2]);
